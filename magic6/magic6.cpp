3427ab2b5dac0deab4363700d3701c403da73fd7
adrianursu <uursu.adrian@gmail.com>
2020-10-28 13:35:24 +0200

    resolved change request #2

--- magic6/magic6.cpp
+++ magic6/magic6.cpp
@@ -5,83 +5,82 @@ using namespace std;
 ifstream input("magic6.in");
 ofstream output("magic6.out");
 
-char character;
 int n;
+char character;
 int frequencyVector[10] = {};
 
 void processCharacters()
 {
-	if (character > 47 && character < 58)
-	{
-		frequencyVector[character - '0'] = 1;
-	}
-	else
-	{
-		output << (char)(character - 32);
-	}
+    if (character > 47 && character < 58)
+    {
+        frequencyVector[character - '0'] = 1;
+    }
+    else
+    {
+        output << (char)(character - 32);
+    }
 }
 
-void verifyFirstDigitAfter0(int &position)
+int getFirstDigitAfterZero()
 {
-	int i = 1;
+    int i = 1;
+    int position = 0;
 
-	while (position < 0)
-	{
-		if (frequencyVector[i] == 1)
-		{
-			position = i;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	output << position << 0;
+    while (position < 1)
+    {
+        if (frequencyVector[i] == 1)
+        {
+            position = i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    output << position << 0;
+
+    return position;
 }
 
-void outputNumbers(int &index)
+void outputNumbers(int index)
 {
-	for (int i = index + 1; i < 10; i++)
-	{
-		if (frequencyVector[i] == 1)
-		{
-			output << i;
-		}
-	}
+    for (int i = index + 1; i < 10; i++)
+    {
+        if (frequencyVector[i] == 1)
+        {
+            output << i;
+        }
+    }
 }
 
 void processNumbers()
 {
-	int poz = -1;
-	int k = 0;
+    int position;
 
-	if (frequencyVector[0] == 1)
-	{
-		verifyFirstDigitAfter0(poz);
-		outputNumbers(poz);
-	}
-	else
-	{
-		outputNumbers(k);
-	}
+    if (frequencyVector[0] == 1)
+    {
+        position = getFirstDigitAfterZero();
+    }
+    outputNumbers(position);
 }
 
 void read()
 {
-	input >> n;
+    input >> n;
 
-	for (int i = 1; i <= n; i++)
-	{
-		input >> character;
-		processCharacters();
-	}
+    for (int i = 1; i <= n; i++)
+    {
+        input >> character;
+        processCharacters();
+    }
 }
 
 int main()
 {
-	read();
-	output << endl;
-	processNumbers();
+    read();
+    output << endl;
+    processNumbers();
 
-	return 0;
+    return 0;
 }
